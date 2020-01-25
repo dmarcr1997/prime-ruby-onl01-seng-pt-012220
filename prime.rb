b847450c69a156540245e7d82b719c00138402ef
Damon R <dmarcr1997@gmail.com>
2020-01-25 00:46:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,12 @@
 require 'pry'
 def prime?(num)
-  range = (2..100).to_a
-  range2 =(-2..-100)
+  range = (-100..100).to_a
   if (num == 2)
     return true
   elsif(num == 1 or num == -1)
     return false
   end
   range.find do |n|
-    if num!=n
-      return false if num% n == 0
-    end
-  end
-   range2.find do |n|
     if num!=n
       return false if num% n == 0
     end
